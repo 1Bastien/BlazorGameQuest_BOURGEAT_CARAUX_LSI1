07f86672823227f7de410dac7a6dc2db5fa2aa6d
Bastien <bgt.bastien@protonmail.com>
2025-10-07 18:04:07 +0200

    feat(AuthenticationServices): enhance application setup by adding service registration and endpoint mapping

--- AuthenticationServices/Program.cs
+++ AuthenticationServices/Program.cs
@@ -1,6 +1,20 @@
+// Création de l'application
 var builder = WebApplication.CreateBuilder(args);
 
+// Ajout des services au conteneur
+builder.Services.AddControllers();
+
+// Ajout des endpoints explorer
+builder.Services.AddEndpointsApiExplorer();
+
+// Création de l'application    
 var app = builder.Build();
 
-app.Run();
+// Utilisation de l'autorisation
+app.UseAuthorization();
 
+// Mapping des controllers
+app.MapControllers();
+
+// Lancement de l'application
+app.Run();
