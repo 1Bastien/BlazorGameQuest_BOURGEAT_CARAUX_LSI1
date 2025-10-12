7f191aa14ea3d38f62e7555fd897f51a94d7afe2
Bastien <bastien.brgt@protonmail.com>
2025-10-12 18:03:44 +0200

    feat: add comment

--- BlazorGame.Client/Program.cs
+++ BlazorGame.Client/Program.cs
@@ -2,10 +2,17 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorGame.Client;
 
+// Création de l'application
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Ajout des composants à la racine
 builder.RootComponents.Add<App>("#app");
+
+// Ajout du head outlet qui sert à injecter la balise head dans le html
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Ajout des services
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// Lancement de l'application
 await builder.Build().RunAsync();
